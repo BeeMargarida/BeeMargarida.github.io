114a8d27734e3596e186e5e1759d3a91e3edb0f6
Margarida Silva <ana.silva@replai.io>
2023-01-28 23:27:35 +0000

    Updates

---
+++ internal/immutable/components/pages/_page.svelte-4bbfaae5.js
@@ -0,0 +1,27 @@
+import{S as z,i as O,s as Q,k as l,q as f,a as N,l as n,m as s,r as p,h as a,c as V,n as i,b as I,D as e,B}from"../../chunks/index-43373e4b.js";function R(D){let c,k,g,v,E,S,o,b,r,x,A,y,u,G,H,w,d,K,M,_,j;return{c(){c=l("p"),k=f(`Hi, I'm Margarida, a software developer. I like to code, learn and do stuff. Hit me up if you have
+	a stupid idea that you would like to see converted to code. Graphic design is not my passion.`),g=N(),v=l("h3"),E=f("What I've been up to"),S=N(),o=l("ul"),b=l("li"),r=l("a"),x=f("SvelteUI"),A=f(`
+	Component and utility library for Svelte and SvelteKit. I'm one of the maintainers, it's still beta,
+	but it's been really cool developing with a community of people giving feedback and using it.
+	`),y=l("li"),u=l("a"),G=f("I would like to contribute"),H=f(`
+	A simple SvelteKit web application that uses the search endpoint of GitHub API to look for small/medium
+	projects that are open to contributions. Not really that useful, but it was cool to try out SvelteKit
+	1.0.
+	`),w=l("li"),d=l("a"),K=f("Video to GIF"),M=f(`
+	A simple SolidJS web application that converts small videos into GIFs. Emphasis on small, since ffmpeg
+	running with WASM cannot handle big videos due to memory limitations. It was more of an excuse to experiment
+	with SolidJS and vent my frustration with BitBucket awful experience when trying to add videos to issues
+	*I'm still mad*.
+	`),_=l("li"),j=f("More stuff not yet ready"),this.h()},l(t){c=n(t,"P",{});var h=s(c);k=p(h,`Hi, I'm Margarida, a software developer. I like to code, learn and do stuff. Hit me up if you have
+	a stupid idea that you would like to see converted to code. Graphic design is not my passion.`),h.forEach(a),g=V(t),v=n(t,"H3",{});var L=s(v);E=p(L,"What I've been up to"),L.forEach(a),S=V(t),o=n(t,"UL",{});var m=s(o);b=n(m,"LI",{});var F=s(b);r=n(F,"A",{href:!0,target:!0,rel:!0,class:!0});var J=s(r);x=p(J,"SvelteUI"),J.forEach(a),F.forEach(a),A=p(m,`
+	Component and utility library for Svelte and SvelteKit. I'm one of the maintainers, it's still beta,
+	but it's been really cool developing with a community of people giving feedback and using it.
+	`),y=n(m,"LI",{});var P=s(y);u=n(P,"A",{href:!0,target:!0,rel:!0,class:!0});var W=s(u);G=p(W,"I would like to contribute"),W.forEach(a),P.forEach(a),H=p(m,`
+	A simple SvelteKit web application that uses the search endpoint of GitHub API to look for small/medium
+	projects that are open to contributions. Not really that useful, but it was cool to try out SvelteKit
+	1.0.
+	`),w=n(m,"LI",{});var C=s(w);d=n(C,"A",{href:!0,target:!0,rel:!0,class:!0});var U=s(d);K=p(U,"Video to GIF"),U.forEach(a),C.forEach(a),M=p(m,`
+	A simple SolidJS web application that converts small videos into GIFs. Emphasis on small, since ffmpeg
+	running with WASM cannot handle big videos due to memory limitations. It was more of an excuse to experiment
+	with SolidJS and vent my frustration with BitBucket awful experience when trying to add videos to issues
+	*I'm still mad*.
+	`),_=n(m,"LI",{});var q=s(_);j=p(q,"More stuff not yet ready"),q.forEach(a),m.forEach(a),this.h()},h(){i(r,"href","https://svelteui.org"),i(r,"target","_blank"),i(r,"rel","noreferrer"),i(r,"class","svelte-1rakjft"),i(u,"href","https://i-would-like-to-contribute.vercel.app/"),i(u,"target","_blank"),i(u,"rel","noreferrer"),i(u,"class","svelte-1rakjft"),i(d,"href","https://video-to-gif-bee.vercel.app/"),i(d,"target","_blank"),i(d,"rel","noreferrer"),i(d,"class","svelte-1rakjft")},m(t,h){I(t,c,h),e(c,k),I(t,g,h),I(t,v,h),e(v,E),I(t,S,h),I(t,o,h),e(o,b),e(b,r),e(r,x),e(o,A),e(o,y),e(y,u),e(u,G),e(o,H),e(o,w),e(w,d),e(d,K),e(o,M),e(o,_),e(_,j)},p:B,i:B,o:B,d(t){t&&a(c),t&&a(g),t&&a(v),t&&a(S),t&&a(o)}}}class X extends z{constructor(c){super(),O(this,c,null,R,Q,{})}}export{X as default};
