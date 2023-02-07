ed71e05095314c0966851a3a60b66c028c9ac475
Margarida Silva <ams-amargarida@hotmail.com>
2023-02-07 19:20:04 +0000

    Updates

--- internal/immutable/chunks/4-de2ccba3.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./_page-e94d8ac7.js";import{default as t}from"../components/pages/cv/_page.svelte-8d7caeb3.js";export{t as component,r as universal};
---
+++ internal/immutable/components/pages/cv/_page.svelte-8e1ce126.js
@@ -0,0 +1,59 @@
+import{S as _t,i as St,s as yt,k as l,q as a,a as p,l as n,m as s,r as o,h as r,c as m,n as i,b as f,D as e,B as at}from"../../../chunks/index-43373e4b.js";function Et(kt){let P,fe,W,v,de,Q,V,he,q,d,T,ue,g,pe,me,ve,R,ge,w,we,be,ke,J,_e,b,Se,ye,Ee,j,Ie,k,Ae,De,Pe,M,Te,_,Re,Je,je,G,z,Me,X,C,L,Ce,S,Le,Ne,Fe,Y,H,xe,Z,h,N,Be,U,Ve,ze,He,y,Ue,Ke,E,Oe,We,$,K,Qe,ee,I,F,qe,A,Ge,Xe,Ye,x,Ze,D,$e,et,tt;return{c(){P=l("p"),fe=a(`My name is Ana Margarida Silva and I am a software engineer. I don’t have any preference on which
+	side of the stack I work on and I’m always willing to learn new things. Currently I’m learning
+	Rust, exploring Svelte, tinkering with some ESP32s and contributing to some awesome open-source
+	projects.`),W=p(),v=l("a"),de=a("CV PDF"),Q=p(),V=l("h2"),he=a("Work"),q=p(),d=l("ul"),T=l("li"),ue=a("Software Engineer @ "),g=l("a"),pe=a("Callstack"),me=a(" [February 2023 - Present]"),ve=p(),R=l("li"),ge=a("Software Engineer @ "),w=l("a"),we=a("Replai"),be=a(" [July 2022 - December 2022]"),ke=a(`
+	Full-stack work on a fast paced startup whose product is a video intelligence platform that uses computer-vision
+	AI to tag creatives at scale and help app developers understand what are the key drivers or detractors
+	of performance of their video creatives. Technologies used are Typescript, NodeJS, React and PostgreSQL.
+	`),J=l("li"),_e=a("Software Engineer @ "),b=l("a"),Se=a("Platforme"),ye=a(" [September 2020 - June 2022]"),Ee=a(`
+	Worked on a startup that used 3D models to power their made-to-order pipeline. The products worked
+	on ranged from a web and mobile app for customizing 3D models (VueJS/React Native), a mobile application
+	to follow and update the order’s status on the factory floor (React Native), the backend to power all
+	of this (Python and NodeJS) and micro-services that complimented with additional functionality (NodeJS,
+	Apache Kafka, Typescript). In addition to this, also developed SDKs to integrate with the platform.
+	`),j=l("li"),Ie=a("Software Engineer Trainee @ "),k=l("a"),Ae=a("Platforme"),De=a(" [October 2019 - September 2020]"),Pe=a(`
+	Worked part-time in several projects, including backend and core services as well as frontend in several
+	products that interface with clients.
+	`),M=l("li"),Te=a("Summer Internship @ "),_=l("a"),Re=a("Blip"),Je=a(" [July 2018 - September 2018]"),je=a(`
+	Development of a testing and inspection tool for serialized content in Apache Kafka topics. Blog post
+	of the experience here.`),G=p(),z=l("h2"),Me=a("Projects"),X=p(),C=l("ul"),L=l("li"),Ce=a("Maintainer and contributor of "),S=l("a"),Le=a("SvelteUI"),Ne=a(" library"),Fe=a(`
+	Maintainer and contributor of a Svelte component and utility library`),Y=p(),H=l("h2"),xe=a("Education"),Z=p(),h=l("ul"),N=l("li"),Be=a("Masters Degree in Informatics and Computing Engineering @ "),U=l("span"),Ve=a("FEUP"),ze=a(` [2015
+		- 2020]`),He=a(`
+	Dissertation in Orchestration for Automatic Decentralization in Visually-defined IoT: Development of
+	a prototype, extending from Node-RED, that enables the automatic decentralization of an IoT system.
+	The devices in the network communicate their computational capabilities to the orchestrator, which
+	is responsible for automatically partitioning the computation and sending the generated tasks to the
+	devices, leveraging their computational power.`),y=l("a"),Ue=a("Thesis"),Ke=a(`
+	and `),E=l("a"),Oe=a("code"),We=a("."),$=p(),K=l("h2"),Qe=a("Publications"),ee=p(),I=l("ul"),F=l("li"),qe=a("Visually-defined Real-Time Orchestration of IoT Systems - "),A=l("a"),Ge=a("10.1145/3448891.3448938"),Xe=a(" [EAI MobiQuitous 2020]"),Ye=a(`
+	By Margarida Silva, João Pedro Dias, André Restivo and Hugo Sereno Ferreira
+	`),x=l("li"),Ze=a("A Review on Visual Programming for Distributed Computation in IoT - "),D=l("a"),$e=a("10.1007/978-3-030-77970-2_34"),et=a(" [ICCS 2021]"),tt=a(`
+	By Margarida Silva, João Pedro Dias, André Restivo and Hugo Sereno Ferreira`),this.h()},l(t){P=n(t,"P",{});var c=s(P);fe=o(c,`My name is Ana Margarida Silva and I am a software engineer. I don’t have any preference on which
+	side of the stack I work on and I’m always willing to learn new things. Currently I’m learning
+	Rust, exploring Svelte, tinkering with some ESP32s and contributing to some awesome open-source
+	projects.`),c.forEach(r),W=m(t),v=n(t,"A",{href:!0,download:!0,class:!0});var ot=s(v);de=o(ot,"CV PDF"),ot.forEach(r),Q=m(t),V=n(t,"H2",{});var it=s(V);he=o(it,"Work"),it.forEach(r),q=m(t),d=n(t,"UL",{});var u=s(d);T=n(u,"LI",{});var te=s(T);ue=o(te,"Software Engineer @ "),g=n(te,"A",{href:!0,target:!0,rel:!0,class:!0});var lt=s(g);pe=o(lt,"Callstack"),lt.forEach(r),me=o(te," [February 2023 - Present]"),te.forEach(r),ve=m(u),R=n(u,"LI",{});var re=s(R);ge=o(re,"Software Engineer @ "),w=n(re,"A",{href:!0,target:!0,rel:!0,class:!0});var nt=s(w);we=o(nt,"Replai"),nt.forEach(r),be=o(re," [July 2022 - December 2022]"),re.forEach(r),ke=o(u,`
+	Full-stack work on a fast paced startup whose product is a video intelligence platform that uses computer-vision
+	AI to tag creatives at scale and help app developers understand what are the key drivers or detractors
+	of performance of their video creatives. Technologies used are Typescript, NodeJS, React and PostgreSQL.
+	`),J=n(u,"LI",{});var ae=s(J);_e=o(ae,"Software Engineer @ "),b=n(ae,"A",{href:!0,target:!0,rel:!0,class:!0});var st=s(b);Se=o(st,"Platforme"),st.forEach(r),ye=o(ae," [September 2020 - June 2022]"),ae.forEach(r),Ee=o(u,`
+	Worked on a startup that used 3D models to power their made-to-order pipeline. The products worked
+	on ranged from a web and mobile app for customizing 3D models (VueJS/React Native), a mobile application
+	to follow and update the order’s status on the factory floor (React Native), the backend to power all
+	of this (Python and NodeJS) and micro-services that complimented with additional functionality (NodeJS,
+	Apache Kafka, Typescript). In addition to this, also developed SDKs to integrate with the platform.
+	`),j=n(u,"LI",{});var oe=s(j);Ie=o(oe,"Software Engineer Trainee @ "),k=n(oe,"A",{href:!0,target:!0,rel:!0,class:!0});var ct=s(k);Ae=o(ct,"Platforme"),ct.forEach(r),De=o(oe," [October 2019 - September 2020]"),oe.forEach(r),Pe=o(u,`
+	Worked part-time in several projects, including backend and core services as well as frontend in several
+	products that interface with clients.
+	`),M=n(u,"LI",{});var ie=s(M);Te=o(ie,"Summer Internship @ "),_=n(ie,"A",{href:!0,target:!0,rel:!0,class:!0});var ft=s(_);Re=o(ft,"Blip"),ft.forEach(r),Je=o(ie," [July 2018 - September 2018]"),ie.forEach(r),je=o(u,`
+	Development of a testing and inspection tool for serialized content in Apache Kafka topics. Blog post
+	of the experience here.`),u.forEach(r),G=m(t),z=n(t,"H2",{});var dt=s(z);Me=o(dt,"Projects"),dt.forEach(r),X=m(t),C=n(t,"UL",{});var rt=s(C);L=n(rt,"LI",{});var le=s(L);Ce=o(le,"Maintainer and contributor of "),S=n(le,"A",{href:!0,target:!0,rel:!0,class:!0});var ht=s(S);Le=o(ht,"SvelteUI"),ht.forEach(r),Ne=o(le," library"),le.forEach(r),Fe=o(rt,`
+	Maintainer and contributor of a Svelte component and utility library`),rt.forEach(r),Y=m(t),H=n(t,"H2",{});var ut=s(H);xe=o(ut,"Education"),ut.forEach(r),Z=m(t),h=n(t,"UL",{});var B=s(h);N=n(B,"LI",{});var ne=s(N);Be=o(ne,"Masters Degree in Informatics and Computing Engineering @ "),U=n(ne,"SPAN",{class:!0});var pt=s(U);Ve=o(pt,"FEUP"),pt.forEach(r),ze=o(ne,` [2015
+		- 2020]`),ne.forEach(r),He=o(B,`
+	Dissertation in Orchestration for Automatic Decentralization in Visually-defined IoT: Development of
+	a prototype, extending from Node-RED, that enables the automatic decentralization of an IoT system.
+	The devices in the network communicate their computational capabilities to the orchestrator, which
+	is responsible for automatically partitioning the computation and sending the generated tasks to the
+	devices, leveraging their computational power.`),y=n(B,"A",{href:!0,target:!0,rel:!0,class:!0});var mt=s(y);Ue=o(mt,"Thesis"),mt.forEach(r),Ke=o(B,`
+	and `),E=n(B,"A",{href:!0,target:!0,rel:!0,class:!0});var vt=s(E);Oe=o(vt,"code"),vt.forEach(r),We=o(B,"."),B.forEach(r),$=m(t),K=n(t,"H2",{});var gt=s(K);Qe=o(gt,"Publications"),gt.forEach(r),ee=m(t),I=n(t,"UL",{});var O=s(I);F=n(O,"LI",{});var se=s(F);qe=o(se,"Visually-defined Real-Time Orchestration of IoT Systems - "),A=n(se,"A",{href:!0,target:!0,rel:!0,class:!0});var wt=s(A);Ge=o(wt,"10.1145/3448891.3448938"),wt.forEach(r),Xe=o(se," [EAI MobiQuitous 2020]"),se.forEach(r),Ye=o(O,`
+	By Margarida Silva, João Pedro Dias, André Restivo and Hugo Sereno Ferreira
+	`),x=n(O,"LI",{});var ce=s(x);Ze=o(ce,"A Review on Visual Programming for Distributed Computation in IoT - "),D=n(ce,"A",{href:!0,target:!0,rel:!0,class:!0});var bt=s(D);$e=o(bt,"10.1007/978-3-030-77970-2_34"),bt.forEach(r),et=o(ce," [ICCS 2021]"),ce.forEach(r),tt=o(O,`
+	By Margarida Silva, João Pedro Dias, André Restivo and Hugo Sereno Ferreira`),O.forEach(r),this.h()},h(){i(v,"href","/cv.pdf"),i(v,"download",""),i(v,"class","svelte-1rakjft"),i(g,"href","https://www.callstack.com/"),i(g,"target","_blank"),i(g,"rel","noreferrer"),i(g,"class","highlight svelte-1rakjft"),i(w,"href","https://replai.io"),i(w,"target","_blank"),i(w,"rel","noreferrer"),i(w,"class","highlight svelte-1rakjft"),i(b,"href","https://platforme.com"),i(b,"target","_blank"),i(b,"rel","noreferrer"),i(b,"class","highlight svelte-1rakjft"),i(k,"href","https://platforme.com"),i(k,"target","_blank"),i(k,"rel","noreferrer"),i(k,"class","highlight svelte-1rakjft"),i(_,"href","https://blip.pt"),i(_,"target","_blank"),i(_,"rel","noreferrer"),i(_,"class","highlight svelte-1rakjft"),i(S,"href","https://svelteui.org"),i(S,"target","_blank"),i(S,"rel","noreferrer"),i(S,"class","svelte-1rakjft"),i(U,"class","highlight"),i(y,"href","https://repositorio-aberto.up.pt/handle/10216/128520"),i(y,"target","_blank"),i(y,"rel","noreferrer"),i(y,"class","svelte-1rakjft"),i(E,"href","https://github.com/BeeMargarida/node-red"),i(E,"target","_blank"),i(E,"rel","noreferrer"),i(E,"class","svelte-1rakjft"),i(A,"href","https://dl.acm.org/doi/10.1145/3448891.3448938"),i(A,"target","_blank"),i(A,"rel","noreferrer"),i(A,"class","svelte-1rakjft"),i(D,"href","https://link.springer.com/chapter/10.1007/978-3-030-77970-2_34"),i(D,"target","_blank"),i(D,"rel","noreferrer"),i(D,"class","svelte-1rakjft")},m(t,c){f(t,P,c),e(P,fe),f(t,W,c),f(t,v,c),e(v,de),f(t,Q,c),f(t,V,c),e(V,he),f(t,q,c),f(t,d,c),e(d,T),e(T,ue),e(T,g),e(g,pe),e(T,me),e(d,ve),e(d,R),e(R,ge),e(R,w),e(w,we),e(R,be),e(d,ke),e(d,J),e(J,_e),e(J,b),e(b,Se),e(J,ye),e(d,Ee),e(d,j),e(j,Ie),e(j,k),e(k,Ae),e(j,De),e(d,Pe),e(d,M),e(M,Te),e(M,_),e(_,Re),e(M,Je),e(d,je),f(t,G,c),f(t,z,c),e(z,Me),f(t,X,c),f(t,C,c),e(C,L),e(L,Ce),e(L,S),e(S,Le),e(L,Ne),e(C,Fe),f(t,Y,c),f(t,H,c),e(H,xe),f(t,Z,c),f(t,h,c),e(h,N),e(N,Be),e(N,U),e(U,Ve),e(N,ze),e(h,He),e(h,y),e(y,Ue),e(h,Ke),e(h,E),e(E,Oe),e(h,We),f(t,$,c),f(t,K,c),e(K,Qe),f(t,ee,c),f(t,I,c),e(I,F),e(F,qe),e(F,A),e(A,Ge),e(F,Xe),e(I,Ye),e(I,x),e(x,Ze),e(x,D),e(D,$e),e(x,et),e(I,tt)},p:at,i:at,o:at,d(t){t&&r(P),t&&r(W),t&&r(v),t&&r(Q),t&&r(V),t&&r(q),t&&r(d),t&&r(G),t&&r(z),t&&r(X),t&&r(C),t&&r(Y),t&&r(H),t&&r(Z),t&&r(h),t&&r($),t&&r(K),t&&r(ee),t&&r(I)}}}class At extends _t{constructor(P){super(),St(this,P,null,Et,yt,{})}}export{At as default};
